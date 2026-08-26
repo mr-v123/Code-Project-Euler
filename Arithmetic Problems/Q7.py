54a93c079b3533ccc836810358e99db7c0c78a9c
Pierre <nampham29122010@gmail.com>
2026-08-26 17:35:50 +0700

    Replace fixed-range loop with while loop to find the 10001st prime dynamically

--- Arithmetic Problems/Q7.py
+++ Arithmetic Problems/Q7.py
@@ -1,14 +1,19 @@
 def isPrime(n):
+    if n < 2:
+        return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
 
 rank = 0
-for k in range(2, 1000000):
+k = 1
+while rank < 10001:
+    k += 1
     if isPrime(k):
         rank += 1
         if rank == 10001:
-            print(k, rank)
+            print(rank, 'prime number is: ', k)
             break
-        
+
+
